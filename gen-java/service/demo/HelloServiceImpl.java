ca2ff5084831c84c8b7d4aa659cd8f5ee2588d32
baiyaguang <baiyaguang@finupgrooup.com>
2018-04-27 16:10:25 +0800

    add java 版 server端

---
+++ gen-java/service/demo/HelloServiceImpl.java
@@ -0,0 +1,10 @@
+import org.apache.thrift.TException;
+
+public class HelloServiceImpl implements Hello.Iface {
+
+	@Override
+	public String helloString(String para) throws TException {
+		return "result: " + para;
+	}
+
+}
